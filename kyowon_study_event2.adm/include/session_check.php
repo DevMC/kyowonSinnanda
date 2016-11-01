3f6cfa433607144598ce0914d6e39bc2d61b42ac
socialmcDev <dev@socialmc.co.kr>
2016-11-01 11:31:28 +0900

    Update session_check.php

--- kyowon_study_event2.adm/include/session_check.php
+++ kyowon_study_event2.adm/include/session_check.php
@@ -1,10 +1,4 @@
 <?
 session_start();
-if($_SESSION["adm_status"] != "admin"){
-	header("Location: ../");
-}else if($_SESSION["adm_status"] == ""){
-	header("Location: ../");
-}else if(empty($_SESSION["adm_status"])){
-	header("Location: ../");
-}
+
 ?>
